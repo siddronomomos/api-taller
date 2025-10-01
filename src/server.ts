6a148d07dc23d77d8cf76f0f23e1c4c215077b34
siddronomomos <christopheralcocer938@gmail.com>
2025-10-01 17:47:04 -0600

    debug info

--- src/server.ts
+++ src/server.ts
@@ -5,4 +5,10 @@ const { port } = env;
 
 app.listen(port, () => {
     console.log(`🚗 API Taller Mecánico escuchando en http://localhost:${port}`);
+    console.log(`⚙️  Modo: ${env.nodeEnv}`);
+    console.log(`💾 Base de datos: ${env.dbName}`);
+    console.log(`🌐 Host: ${env.dbHost}`);
+    console.log(`👤 Usuario: ${env.dbUser}`);
+    console.log(`🔢 Puerto de la base de datos: ${env.dbPort}`);
+
 });
