482fe145ac0dbba8c9310d69440201086304a481
siddronomomos <christopheralcocer938@gmail.com>
2025-10-01 08:06:31 -0600

    im dum

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -4,7 +4,7 @@ import { env } from '../config/env';
 export const pool = mysql.createPool({
     host: env.dbHost,
     user: env.dbUser,
-    port: env.port,
+    port: env.dbPort,
     password: env.dbPassword,
     database: env.dbName,
     waitForConnections: true,
