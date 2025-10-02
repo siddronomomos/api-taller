f7219b100ed79646379057392fda33afd3027055
siddronomomos <christopheralcocer938@gmail.com>
2025-10-01 20:01:39 -0600

    change server logs

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,10 @@ import { env } from './config/env';
 const { port } = env;
 
 app.listen(port, () => {
-    console.log(`🚗 API Taller Mecánico escuchando en http://localhost:${port}`);
-    console.log(`⚙️  Modo: ${env.nodeEnv}`);
-    console.log(`💾 Base de datos: ${env.dbName}`);
-    console.log(`🌐 Host: ${env.dbHost}`);
-    console.log(`👤 Usuario: ${env.dbUser}`);
-    console.log(`🔢 Puerto de la base de datos: ${env.dbPort}`);
-
+    console.log(`API Taller Mecánico en http://140.84.169.148:${port}`);
+    console.log(`Modo: ${env.nodeEnv}`);
+    console.log(`Base de datos: ${env.dbName}`);
+    console.log(`Host: ${env.dbHost}`);
+    console.log(`Usuario: ${env.dbUser}`);
+    console.log(`Puerto de la base de datos: ${env.dbPort}`);
 });
